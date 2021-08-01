3e5f683b0169142c45d30a223aa52c3827845d59
Darren.Cheng <chuhancheng@gmail.com>
2021-08-02 00:09:17 +0800

    Finalize SSR, SSG tutorial

---
+++ pages/posts/first-post.js
@@ -0,0 +1,23 @@
+import Link from "next/link"
+import Head from "next/head"
+import Layout from '../../components/layout'
+
+export async function getServerSideProps(context) {
+  console.log('YOOO');
+  return {
+    props: {
+      mydata: 123
+    }
+  }
+}
+
+export default function FirstPost({mydata}) {
+  return (
+    <Layout home={false}>
+      <Head>
+        <title>First Post: ({mydata})</title>
+      </Head>
+      <h1>First Post</h1>
+    </Layout>
+  )
+}
