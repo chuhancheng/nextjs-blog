1110037064bdf7ac04f8bb3257d595df48dc938b
Darren.Cheng <chuhancheng@gmail.com>
2021-08-04 00:39:12 +0800

    Fix indent

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -3,21 +3,21 @@ import Head from "next/head"
 import Layout from '../../components/layout'
 
 export async function getServerSideProps(context) {
-  console.log('YOOO');
-  return {
-    props: {
-      mydata: 123
+    console.log('YOOO');
+    return {
+        props: {
+            mydata: 123
+        }
     }
-  }
 }
 
 export default function FirstPost({mydata}) {
-  return (
-    <Layout home={false}>
-      <Head>
-        <title>First Post: ({mydata})</title>
-      </Head>
-      <h1>First Post</h1>
-    </Layout>
-  )
+    return (
+        <Layout home={false}>
+            <Head>
+                <title>First Post: ({mydata})</title>
+            </Head>
+            <h1>First Post</h1>
+        </Layout>
+    )
 }
